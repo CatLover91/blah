e9836570add41fb82f8605ba9b9a4f1fc4382278
Nicholai Goodall <Kranoscorp@gmail.com>
2014-10-13 02:17:02 -0400

    Build is still broken >.> changed more framework stuff

--- genAlg.js
+++ genAlg.js
@@ -19,21 +19,28 @@ Design a genetic algorithm to obtain an optimal ranking. Specify your setup of t
 mutation operator, and crossover operator.
 */
 //initialize
-function Gene(leng, teamamount) {// this algorithm will be turned into a Jags ranking algorithm
-    var geneLength = leng;
-    var name = "";
-    var fit = this.fitness();
-    var teamnum = teamamount;
-    var gstring = this.init(teamamount); //hahahah
+function Gene(leng) { // this algorithm will be turned into a Jags ranking algorithm
+    var result = [];
+    for (var i = 0; i < leng; i++) {
+        var checker = true;
+        var newb;
+        if (i !== 0)
+            while (checker) {
+                newb = math.floor(math.random() * leng);
+                checker = false;
+                for (var q = i - 1; q > 0; q--) {
+                    if (newb === result[q]) checker = true;
+                }
+            }
+        else result.push(math.floor(math.random() * leng));
+        result.push(newb);
+    }
+    //console.log(result.toString());
+    this.geneLength = leng;
+    this.teamnum = leng;
+    this.gstring = result; //hahahah
+    this.fit = this.fitness();
 }
-
-Gene.prototype.init = function(numbOfTeams) { //initialize gene as a random 10-digit binary number
-    var result = "";
-    for (var i = 0; i < this.geneLength; i++) {
-        result.concat((math.random() % numbOfTeams).toString)
-    return result;
-};
-
 
 Gene.prototype.mutate = function(mutationchance) {
     if (mutationchance === undefined) { //mutation chance auto-selects 10%
@@ -41,11 +48,14 @@ Gene.prototype.mutate = function(mutationchance) {
     }
     for (var i = 0; i < this.gstring.length; i++) {
         if (math.random() < mutationchance) {
-            var newval = (math.random() % this.teamnum);
-            while(this.gstring.charAt(i) === newval.toString()) {
-                newval = (math.random() % this.teamnum);
+            var newval = math.floor(math.random() * this.teamnum);
+            while (this.gstring[i] === newval) {
+                newval = math.floor(math.random() * this.teamnum);
             }
-            this.gstring = this.gstring(0, i) + newval.toString() + this.gstring(i + 1);
+            for (var q = 0; q < this.teamnum; q++) {
+                if (this.gstring[q] === newval) this.gstring[q] === this.gstring[i];
+            }
+            this.gstring[i] = newval; //this.gstring(0, i) + newval.toString() + this.gstring(i + 1);
         }
     }
 };
@@ -53,69 +63,47 @@ Gene.prototype.mutate = function(mutationchance) {
 
 Gene.prototype.cross = function(gene) {
     var swappoint = math.floor(math.random() * this.gstring.length);
-
-    var child1 = this.gstring.substr(0, swappoint) + gene.gstring.substr(swappoint);
-    var child2 = gene.gstring.substr(0, swappoint) + this.gstring.substr(swappoint);
-
-    return [child1, child2];
+    var child = [];
+    for (var i = 0; i < swappoint; i++) {
+        child.push(this.gstring[i]);
+        this.gstring[i] = gene[i];
+    }
+    for (i = swappoint + 1; i < gene.length; i++) {
+        child.push(gene[i]);
+    }
+    return child;
 };
 
 // fitness ex : ln(P(a>b)+ln(P(b>c))
 Gene.prototype.fitness = function() { //converts gene fitness into an int for easy comparison
-    var result = 0;
-    var possibilities = [];
-    for (var i = 0; i < this.gstring.length - this.teamnum; i++) {
-        var temp = "";
-        var checker = true;
-        for(var j = 0; j < this.teamnum; j++) {
-            temp.concat(this.gstring.charAt(i+j));
+    var newb = 1;
+    for (var i = 0; i < this.gstring.length - 1; i++) {
+        if (rules[this.gstring[i]][this.gstring[i + 1]] !== "----") {
+            newb = newb * math.log(rules[this.gstring[i]][this.gstring[i + 1]]);
         }
-        for(var q = 1; q < temp.length; q++) {
-            for(var p = q - 1; p >= 0; p--) {
-                if(temp.charAt(q) === temp.charAt(p)) {
-                    checker = false;
-                }
-            }
-        }
-        if(checker) possibilities.push(temp);
+        else newb = newb * math.log(rules[this.gstring[i + 1]][this.gstring[i]]);
     }
-    
-    for(var m = 0; m < possibilities.length; m++) {
-        var newb = this.rankPercentage(possibilities[m]);
-        if(result === 0 || newb < result) {
-            result = newb;
-            this.name = possibilities[m];
-        }
-    }
-    return result;
+    return newb;
 };
 
 //-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------
 
-function Population(Probabilities, Cross, stringlength, teamamount, popTotal) {
-    var genes = [];
-    var CR = Cross;
-    var rules = Probabilities;
-    
-    for(var i = 0; i < popTotal; i++) {
-        genes.push(new Gene(stringlength, teamamount));
-    }
-}
+function Population(Probabilities, Cross, teamamount, popTotal) {
+    this.genes = [];
+    this.CR = Cross;
+    this.rules = Probabilities;
 
-Population.prototype.rankPercentage = function(teams) {
-    var result = 1;
-    for (var i = 0; i < teams.length - 2; i++) {
-        result = result * math.log(this.rules[i][i + 1]);
+    for (var i = 0; i < popTotal; i++) {
+        this.genes.push(new Gene(teamamount));
     }
-    return result;
 }
 
 Population.prototype.sort = function() {
-    for(var i = 0; i < this.genes.length; i++) {
-        for(var j = i; j < this.genes.length; j++) {
-            if(this.genes[i].fitness() > this.genes[j].fitness()) {
+    for (var i = 0; i < this.genes.length; i++) {
+        for (var j = i; j < this.genes.length; j++) {
+            if (this.genes[i].fitness() > this.genes[j].fitness()) {
                 var temp = this.genes[i];
                 this.genes[i] = this.genes[j];
                 this.genes[j] = temp;
@@ -126,94 +114,59 @@ Population.prototype.sort = function() {
 
 
 Population.prototype.runAround = function() { //WHYYYY you wanna give me tha run arouuund
-    
+
     //apply fitness proportion selection
     this.sort();
-    
+
     var fitness = [];
     var counter = 0;
-    for(var i = 0; i < this.genes.length; i++) {
-        fitness.push(this.genes[i].fitness);
-        counter += this.genes[i].fitness;
+    for (var i = 0; i < this.genes.length; i++) {
+        fitness.push(this.genes[i].fitness());
+        counter += this.genes[i].fitness();
     }
-    for(var i = 0; i < this.genes.length; i++) {
-        if(i === 0) {
+
+    for (var i = 0; i < this.genes.length; i++) {
+        if (i === 0) {
             fitness[i] = fitness[i] / counter;
-        } else {
+        }
+        else {
             fitness[i] = (fitness[i] / counter) + fitness[i - 1];
         }
     }
-    
     //match for crossover
     var crossOverMatch = [];
     var whoIsLucky = 0;
-    for(var i = 0; i < this.genes.length; i++) {
+    for (var i = 0; i < this.genes.length; i++) {
         whoIsLucky = math.random();
-        for(var j = 0; j < fitness.length; j++) {
-            if(whoIsLucky < fitness[j] && (j === 0 || (whoIsLucky > fitness[j - 1]))) {
+        for (var j = 0; j < fitness.length; j++) {
+            if (whoIsLucky < fitness[j] && (j === 0 || (whoIsLucky > fitness[j - 1]))) {
                 crossOverMatch.push(j);
             }
         }
     }
-    
+    console.log(crossOverMatch.toString());
     // perform crossover
-    for(var i = 0; i < fitness.length; i++) {
-        if(math.random() < this.CR) {
-            var newbees = this.genes[i].cross(this.genes[crossOverMatch[i]]);
-            this.genes[i].gstring = newbees[0].gstring;
-            this.genes[crossOverMatch[i]].gstring = newbees[1].gstring;
+    for (var i = 0; i < fitness.length; i++) {
+        if (math.random() < this.CR) {
+            this.genes[crossOverMatch[i]].gstring = this.genes[i].cross(this.genes[crossOverMatch[i]].gstring);
         }
     }
-    
+
     // mutate
-    for(var i = 0; i < this.genes.length; i++) {
+    for (var i = 0; i < this.genes.length; i++) {
         this.genes[i].mutate();
     }
 };
 
 Population.prototype.run = function() {
     console.log("Let's play a game of football...");
-    for(var runAmount = 0; runAmount < 1000; runAmount++) {
+    for (var runAmount = 0; runAmount < 1000; runAmount++) {
         this.sort();
         console.log("Season #" + runAmount);
         console.log("Current Prediction: " + this.genes[0].name);
         this.runAround();
     }
 };
- 
- 
-/*------------------------------------------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------------------------------------------------------------------------------------------------*/
-
-var rules = [];
-var dataToParse = "";
-//read in text
-// Make sure we got a filename on the command line.
-if (process.argv.length < 3) {
-  console.log('Usage: node ' + process.argv[1] + ' FILENAME');
-  process.exit(1);
-}
-// Read the file and print its contents.
-var fs = require('fs'),
-    filename = process.argv[2];
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ' + filename);
-  dataToParse = data;
-});
-
-///parse text
-for(var i = 0; i < dataToParse.length; i++) {
-    if(dataToParse[i] === "P")
-}
-
-var crossoverchance = 0.5;
-//Probabilities, Cross, stringlength, teamamount, popTotal
-var letsPlayAGame = Population(rules, crossoverchance, 10, 4, 20);
-Population.run();
-
-///store data
 
 /*
 Task 2. (Implementation) (20 pts)
@@ -221,12 +174,45 @@ Implement your genetic algorithm given the following ranking probability matrix 
 you obtain. (Hint: Multiplication of probabilities many times may lead to an extremely small value. Apply logarithm to the probability 
 may solve this problem).
 */
-//Genetic algorithm
-///Encoding principles  (gene, chromosome)
-///Initialization procedure (creation)
-///Selection of parents  (reproduction)
-///Genetic operators (mutation, crossover)
-///Evaluation function (fitness to environment)
-///Termination condition
 
-//output
+/*------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+var rules = [];
+var names = [];
+var dataToParse = "";
+//read in text
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
+}
+// Read the file and print its contents.
+var fs = require('fs'),
+    filename = process.argv[2];
+fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    dataToParse = data;
+
+
+    ///parse text
+
+    dataToParse = dataToParse.split("\n");
+    for (var i = 1; i < dataToParse.length; i++) {
+        var temp = dataToParse[i].split(" ");
+        var newRow = [];
+        names.push(temp[0]);
+        for (var j = 1; j < temp.length; j++) {
+            newRow.push(temp[j]);
+        }
+        rules.push(newRow);
+    }
+
+    var crossoverchance = 0.5;
+    //Probabilities, Cross, stringlength, teamamount, popTotal
+    var letsPlayAGame = new Population(rules, crossoverchance, (rules.length * 4), rules.length, 20);
+    letsPlayAGame.run();
+
+});
